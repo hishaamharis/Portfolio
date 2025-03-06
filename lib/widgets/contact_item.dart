f705c2cd00d40622a14ee32e48a6e6bb5629b3ee
hishaamharis <muhammadhisham231@gmail.com>
2025-03-06 12:38:57 +0530

    Improve ContactItem UI: Add hover & tap effects, fix transform issue, and make it responsive

--- lib/widgets/contact_item.dart
+++ lib/widgets/contact_item.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class ContactItem extends StatelessWidget {
+class ContactItem extends StatefulWidget {
   final SvgPicture icon;
   final String label;
   final String value;
@@ -16,27 +16,87 @@ class ContactItem extends StatelessWidget {
   });
 
   @override
+  _ContactItemState createState() => _ContactItemState();
+}
+
+class _ContactItemState extends State<ContactItem> {
+  bool _isHovered = false;
+  bool _isTapped = false;
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        width: 220,
-        height: 150,
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.1),
-          borderRadius: BorderRadius.circular(16),
-          border: Border.all(color: Colors.white.withOpacity(0.2)),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            icon,
-            const SizedBox(height: 12),
-            Text(label, style: const TextStyle(fontSize: 16, color: Colors.white70)),
-            const SizedBox(height: 6),
-            Text(value, style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white), textAlign: TextAlign.center),
-          ],
+    double screenWidth = MediaQuery.of(context).size.width;
+    bool isMobile = screenWidth < 600;
+
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: GestureDetector(
+        onTapDown: (_) => setState(() => _isTapped = true),
+        onTapUp: (_) {
+          setState(() => _isTapped = false);
+          widget.onTap();
+        },
+        onTapCancel: () => setState(() => _isTapped = false),
+        child: Transform.scale(
+          scale: _isTapped ? 0.96 : 1.0,
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.easeInOut,
+            width: isMobile ? screenWidth * 0.8 : 220,
+            height: isMobile ? 120 : 150,
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: _isHovered
+                  ? Colors.white.withOpacity(0.15)
+                  : Colors.white.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(
+                color: _isHovered
+                    ? Colors.blueAccent.withOpacity(0.4)
+                    : Colors.white.withOpacity(0.2),
+                width: 1.5,
+              ),
+              boxShadow: _isHovered
+                  ? [
+                      BoxShadow(
+                        color: Colors.blueAccent.withOpacity(0.3),
+                        blurRadius: 12,
+                        spreadRadius: 3,
+                        offset: const Offset(3, 6),
+                      ),
+                    ]
+                  : [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.15),
+                        blurRadius: 6,
+                        spreadRadius: 2,
+                        offset: const Offset(2, 4),
+                      ),
+                    ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                widget.icon,
+                const SizedBox(height: 12),
+                Text(
+                  widget.label,
+                  style: const TextStyle(fontSize: 16, color: Colors.white70),
+                ),
+                const SizedBox(height: 6),
+                Text(
+                  widget.value,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
