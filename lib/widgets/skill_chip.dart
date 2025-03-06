8fadd4a2f57985ad85d815a4c8ad64ef06715dbf
hishaamharis <muhammadhisham231@gmail.com>
2025-03-06 11:13:43 +0530

    Enhance SkillChip: Add tap effect for mobile users

--- lib/widgets/skill_chip.dart
+++ lib/widgets/skill_chip.dart
@@ -16,41 +16,50 @@ class SkillChip extends StatefulWidget {
 }
 
 class _SkillChipState extends State<SkillChip> {
-  bool isHovered = false;
+  bool isHoveredOrPressed = false;
+
+  void _updateState(bool newState) {
+    setState(() => isHoveredOrPressed = newState);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      onEnter: (_) => setState(() => isHovered = true),
-      onExit: (_) => setState(() => isHovered = false),
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 200),
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-        decoration: BoxDecoration(
-          color: isHovered ? Colors.teal.shade600 : const Color(0xFF8CA0C6),
-          borderRadius: BorderRadius.circular(20),
-          border: isHovered ? Border.all(color: Colors.white, width: 2) : null, // Border on hover
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 4,
-              offset: Offset(2, 2),
-            ),
-          ],
-        ),
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            if (widget.icon != null) ...[
-              SizedBox(
-                width: 20,
-                height: 20, 
-                child: widget.icon!,
+    return GestureDetector(
+      onTapDown: (_) => _updateState(true), // Effect on tap
+      onTapUp: (_) => _updateState(false), // Remove effect after tap
+      onTapCancel: () => _updateState(false), // Handle tap cancel
+      child: MouseRegion(
+        onEnter: (_) => _updateState(true),
+        onExit: (_) => _updateState(false),
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+          decoration: BoxDecoration(
+            color: isHoveredOrPressed ? Colors.teal.shade600 : const Color(0xFF8CA0C6),
+            borderRadius: BorderRadius.circular(20),
+            border: isHoveredOrPressed ? Border.all(color: Colors.white, width: 2) : null, 
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 4,
+                offset: Offset(2, 2),
               ),
-              const SizedBox(width: 5),
             ],
-            Text(widget.label, style: const TextStyle(color: Colors.white)),
-          ],
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (widget.icon != null) ...[
+                SizedBox(
+                  width: 20,
+                  height: 20, 
+                  child: widget.icon!,
+                ),
+                const SizedBox(width: 5),
+              ],
+              Text(widget.label, style: const TextStyle(color: Colors.white)),
+            ],
+          ),
         ),
       ),
     );
