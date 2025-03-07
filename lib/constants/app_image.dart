e0d2a18dd03366a5a8953ccce2db1c8529ee5d5e
hishaamharis <muhammadhisham231@gmail.com>
2025-03-08 00:37:08 +0530

    update image name

--- lib/constants/app_image.dart
+++ lib/constants/app_image.dart
@@ -2,6 +2,6 @@ class AppImage {
   static const String meImage = "assets/me.jpg";
   static const String projectsmateImage = "assets/images/projectsmate.jpg";
   static const String driveDuoImage = "assets/images/drive_duo.jpg";
-  static const String quizizImage = "assets/images/quiziz.jpg";
+  static const String quizizImage = "assets/images/quizziz.jpg";
   static const String taskManagerImage = "assets/images/task_manager.jpg";
 } 
