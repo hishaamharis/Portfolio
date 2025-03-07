6b488ead595c13ca69bc7fa738b325d8530c10cf
hishaamharis <muhammadhisham231@gmail.com>
2025-03-07 12:07:16 +0530

    feat : Added project images

--- lib/constants/app_image.dart
+++ lib/constants/app_image.dart
@@ -1,4 +1,7 @@
 class AppImage {
   static const String meImage = "assets/me.jpg";
   static const String projectsmateImage = "assets/images/projectsmate.jpg";
-}
+  static const String driveDuoImage = "assets/images/drive_duo.jpg";
+  static const String quizizImage = "assets/images/quiziz.jpg";
+  static const String taskManagerImage = "assets/images/task_manager.jpg";
+} 
